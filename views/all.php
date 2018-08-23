cb792c2f70f3ab562b5076d141d5cd1fc1f5055d
Alex-Undr <golybua@gmail.com>
2018-08-23 15:52:18 +0300

    new styles of table

--- views/all.php
+++ views/all.php
@@ -22,58 +22,33 @@
 			padding: 22px 33px 24px;
 			position: relative;
 		}
-		.table_blur {
-		  background: #f5ffff;
-		  border-collapse: collapse;
-		  text-align: center;
-		  width: 100%;
-		}
-		.table_blur th {
-		  border-top: 1px solid #777777;	
-		  border-bottom: 1px solid #777777; 
-		  box-shadow: inset 0 1px 0 #999999, inset 0 -1px 0 #999999;
-		  background: linear-gradient(#9595b6, #5a567f);
-		  color: white;
-		  padding: 10px 15px;
-		  position: relative;
-		}
-		.table_blur th:after {
-		  content: "";
-		  display: block;
-		  position: absolute;
-		  left: 0;
-		  top: 25%;
-		  height: 25%;
-		  width: 100%;
-		  background: linear-gradient(rgba(255, 255, 255, 0), rgba(255,255,255,.08));
-		}
-		.table_blur tr:nth-child(odd) {
-		  background: #ebf3f9;
-		}
-		.table_blur th:first-child {
-		  border-left: 1px solid #777777;	
-		  border-bottom:  1px solid #777777;
-		  box-shadow: inset 1px 1px 0 #999999, inset 0 -1px 0 #999999;
-		}
-		.table_blur th:last-child {
-		  border-right: 1px solid #777777;
-		  border-bottom:  1px solid #777777;
-		  box-shadow: inset -1px 1px 0 #999999, inset 0 -1px 0 #999999;
-		}
-		.table_blur td {
-		  border: 1px solid #e3eef7;
-		  padding: 10px 15px;
-		  position: relative;
-		  transition: all 0.5s ease;
-		}
-		.table_blur tbody:hover td {
-		  color: transparent;
-		  text-shadow: 0 0 3px #a09f9d;
-		}
-		.table_blur tbody:hover tr:hover td {
-		  color: #444444;
-		  text-shadow: none;
+		
+		.divtable{
+			border: 1px solid black;
+			border-radius: 17px;
 		}		
+		.table_blur {
+			border-collapse: collapse;		
+			margin: auto;
+			width: 100%;
+		}		
+		td{			
+			padding: 10px;
+		}
+		td:nth-child(1n+2){			
+			border-left: 1px solid black;
+		}
+		tr:nth-last-child(1n+2){		
+			border-bottom: 1px solid black;
+		}
+		th{			
+			padding: 10px;
+			border-bottom: 1px solid black;			
+		}
+		th:nth-child(1n+2){			
+			border-left: 1px solid black;
+		}
+
 
         </style>
     </head>
@@ -81,22 +56,24 @@
 <body>
 	<div class="border">
 		<div class="content">
-			<table class="table_blur">
-			  <tr>
-				<th>№</th>
-				<th>Code</th>
-				<th>Date</th>
-			  </tr>
-			<?php $i = 0;
-			foreach($all as $result){
-				$i++;?>			
-			  <tr>
-				<td><?php echo $i;?></td>
-				<td><?php echo $result['code'];?></td>
-				<td><?php echo $result['date'];?></td>
-			  </tr>
-			<?php }	?>
-			</table>
+			<div class="divtable">
+				<table class="table_blur">
+				  <tr>
+					<th>№</th>
+					<th>Code</th>
+					<th>Date</th>
+				  </tr>
+				<?php $i = 0;
+				foreach($all as $result){
+					$i++;?>			
+				  <tr>
+					<td><?php echo $i;?></td>
+					<td><?php echo $result['code'];?></td>
+					<td><?php echo $result['date'];?></td>
+				  </tr>
+				<?php }	?>
+				</table>
+			</div>	
 		</div>
 	</div>
 
